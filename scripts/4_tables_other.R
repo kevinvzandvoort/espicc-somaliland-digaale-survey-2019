89cf34379a2c76015fd040fc93c3c057fb2831ca
Kevin van Zandvoort <Kevin.Van-Zandvoort@lshtm.ac.uk>
2021-08-20 13:20:56 +0100

    Reorder and relabel tables

--- scripts/4_tables_other.R
+++ scripts/4_tables_other.R
@@ -1,22 +1,46 @@
 #' Create table 2: the reported frequency of travel outside of Digaale IDP camp
-table2_travel = svyMean2(~contacts_total+home_contacts+school_or_work_contacts+other_contacts+contacts_other_median+
-                      physical_contacts, contact_data_frequency_ps, na.rm=T, by="participant_age_group_sample",
-                    digits=1)
+table2_travel = svyMean2(~travel_0_5km+travel_5_10km+travel_10km_gt, contact_data_frequency_ps, na.rm=T, digits=1,
+                         multiply = 100) %>% .[!option %in% c("don’t know", "refuse to answer")]
+
 table2_travel[, ci95_low := pmax(0, ci95_low)]
+table2_travel[, mean_label := sprintf("%s%%", mean)]
 table2_travel[, confint := sprintf("%s - %s", ci95_low, ci95_high)]
-table2_travel[, variable := factor(variable, c("contacts_total", "home_contacts", "school_or_work_contacts",
+table2_travel[, variable := factor(variable, c("travel_0_5km", "travel_5_10km", "travel_10km_gt"),
+                                   c("< 5km", "5 - 10km", "> 10km"))]
+
+table2_travel = table2_travel %>%
+  dcast(variable~option, value.var=c("mean_label", "confint")) %>%
+  .[, c("variable", sapply(c("Most days of the week", "At least once per week", "At least once per month",
+                             "Less than once per month", "Never"),
+                           function(x) paste0(c("mean_label_", "confint_"), x))), with=F]
+
+table2_travel %>%
+  kblOut(booktabs=T, align=c("l|","r","l|","r", "l|","r", "l|","r", "l|","r", "l"), linesep = "", col.names=NULL,
+         other_functions = list(
+           function(x) add_header_above(x, c(`Travel distance` = 1, `Most days of the week` = 2,
+                                             `At least once per week` = 2, `At least once per month` = 2,
+                                             `Less than once per month` = 2, `Never` = 2))),
+         out_name = "table2_travel")
+
+#' Create table 3: mean number of reported daily contacts by age, contact type and contact setting
+table3_contacts = svyMean2(~contacts_total+home_contacts+school_or_work_contacts+other_contacts+contacts_other_median+
+                             physical_contacts, contact_data_frequency_ps, na.rm=T, by="participant_age_group_sample",
+                           digits=1)
+table3_contacts[, ci95_low := pmax(0, ci95_low)]
+table3_contacts[, confint := sprintf("%s - %s", ci95_low, ci95_high)]
+table3_contacts[, variable := factor(variable, c("contacts_total", "home_contacts", "school_or_work_contacts",
                                                "other_contacts", "physical_contacts", "contacts_other_median"),
                                    c("Total", "Home or another house", "School or work", "Other", "Physical",
                                      "Indirect"))]
 
-table2_travel = table2_travel %>%
+table3_contacts = table3_contacts %>%
   dcast(participant_age_group_sample~variable, value.var=c("mean", "confint")) %>%
   .[, c("participant_age_group_sample",
         sapply(c("Total", "Physical", "Indirect", "Home or another house", "School or work", "Other"),
                function(x) paste0(c("mean_", "confint_"), x))), with=F]
 
 #' Write the table to the output folder
-table2_travel %>%
+table3_contacts %>%
   kblOut(booktabs=T, formats=c(pdf="latex"),
          align=c("l|","r","l|","r", "l|","r", "l|","r", "l|","r", "l|","r", "l|"), linesep = "", col.names=NULL,
          other_functions =
@@ -24,9 +48,9 @@ table2_travel %>%
                                                   `Total (Indirect)` = 2, `Home or another house` = 2,
                                                   `School or work` = 2, `Other` = 2)),
                 function(x) add_header_above(x, c(` ` = 1, `Contact type` = 6, `Contact setting (Direct)` = 6))),
-         out_name = "table2_travel")
+         out_name = "table3_contacts")
 
-table2_travel %>%
+table3_contacts %>%
   kblOut(booktabs=T, formats=c(html="html"),
          align=c("l|","r","l|","r", "l|","r", "l|","r", "l|","r", "l|","r", "l|"), linesep = "", col.names=NULL,
          other_functions =
@@ -34,36 +58,12 @@ table2_travel %>%
                                                   `Total (Indirect)` = 2, `Home or another house` = 2,
                                                   `School or work` = 2, `Other` = 2)),
                 function(x) add_header_above(x, c(` ` = 1, `Contact type` = 3, `Contact setting (Direct)` = 3))),
-         out_name = "table2_travel")
-
-#' Create table 3: mean number of reported daily contacts by age, contact type and contact setting
-table3_contacts = svyMean2(~travel_0_5km+travel_5_10km+travel_10km_gt, contact_data_frequency_ps, na.rm=T, digits=1,
-                           multiply = 100) %>% .[!option %in% c("don’t know", "refuse to answer")]
-
-table3_contacts[, ci95_low := pmax(0, ci95_low)]
-table3_contacts[, mean_label := sprintf("%s%%", mean)]
-table3_contacts[, confint := sprintf("%s - %s", ci95_low, ci95_high)]
-table3_contacts[, variable := factor(variable, c("travel_0_5km", "travel_5_10km", "travel_10km_gt"),
-                                     c("< 5km", "5 - 10km", "> 10km"))]
-
-table3_contacts = table3_contacts %>%
-  dcast(variable~option, value.var=c("mean_label", "confint")) %>%
-  .[, c("variable", sapply(c("Most days of the week", "At least once per week", "At least once per month",
-                             "Less than once per month", "Never"),
-                           function(x) paste0(c("mean_label_", "confint_"), x))), with=F]
-
-table3_contacts %>%
-  kblOut(booktabs=T, align=c("l|","r","l|","r", "l|","r", "l|","r", "l|","r", "l"), linesep = "", col.names=NULL,
-         other_functions = list(
-           function(x) add_header_above(x, c(`Travel distance` = 1, `Most days of the week` = 2,
-                                             `At least once per week` = 2, `At least once per month` = 2,
-                                             `Less than once per month` = 2, `Never` = 2))),
          out_name = "table3_contacts")
 
 #' Create Supplemental table C1:
 #' - Average daily number of (direct) contacts reported at school or work settings, excluding individuals who reported
 #'   no school and work contacts
-#' - This is a sensitivity analysis of estimates in table 2
+#' - This is a sensitivity analysis of estimates in table 3
 table_sC1_school_work_sens = svyMean2(~school_or_work_contacts,
                                       subset(contact_data_frequency_ps, school_or_work_contacts > 0),
                                       by="participant_age_group_sample", na.rm=T, digits=1)
